a94bd509811ac196564d6c44ceb88a6c2ecb17ac
Ryan Collins <ryanjcollins41@gmail.com>
2022-07-24 19:48:03 -0400

    features update

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Chat from "./components/Chat/Chat";
-// import Cars from "./components/Cars/Cars";
-// import OuterSpace from "./components/OuterSpace/OuterSpace";
 import "./App.css";
 
 function App() {
   const [room, setRoom] = useState("main");
-  const [roomName, setRoomName] = useState('')
-  useEffect(()=>{
-    if (room==='cars'){ setRoomName('Car Talk chat room')}
-    else if (room==='outerspace'){ setRoomName('Outer Space chat room')}
-    else { setRoomName('Main chat room')}
-  }, [room])
-    
+  const [roomName, setRoomName] = useState("Main");
   return (
     <Router>
-      <h1>You are in the {roomName} </h1>
+      <h2>You are in the {roomName} chat room </h2>
       <nav>
-        <Link to="/" onClick={() => setRoom("main")}>
+        <Link
+          to="/"
+          onClick={() => {
+            setRoom("main");
+            setRoomName("Main ");
+          }}
+        >
           Main Chat
         </Link>
-        <Link to="/cars" onClick={() => setRoom("cars")}>
+        <Link
+          to="/cars"
+          onClick={() => {
+            setRoom("cars");
+            setRoomName("Car Talk");
+          }}
+        >
           Car Talk
         </Link>
-        <Link to="/outerspace" onClick={() => setRoom("outerspace")}>
+        <Link
+          to="/outerspace"
+          onClick={() => {
+            setRoom("outerspace");
+            setRoomName("Outer Space");
+          }}
+        >
           Outer Space
         </Link>
       </nav>
       <Routes>
         <Route
           path="/"
-          element={<Chat room={room} fetchPath='main' />}
+          element={
+            <Chat
+              room={room}
+              setRoom={setRoom}
+              setRoomName={setRoomName}
+              fetchPath="main"
+            />
+          }
         />
         <Route
           path="/cars"
-          element={<Chat room={room} fetchPath='cars' />}
+          element={
+            <Chat
+              room={room}
+              setRoom={setRoom}
+              setRoomName={setRoomName}
+              fetchPath="cars"
+            />
+          }
         />
         <Route
           path="/outerspace"
-          element={<Chat room={room} fetchPath='outerspace' />}
+          element={
+            <Chat
+              room={room}
+              setRoom={setRoom}
+              setRoomName={setRoomName}
+              fetchPath="outerspace"
+            />
+          }
         />
       </Routes>
     </Router>
--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,79 +1,130 @@
-import React, { useState, useEffect } from 'react'
-import './Chat.css'
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
 
 function Home(props) {
-  console.log(props.roomMessages)
-  console.log(props.room)
-
-  const [ user, setUser ] = useState("")
-  const [ body, setBody ] = useState("")
-  const [render, setRender] = useState(true)
+  console.log(props.fetchPath);
+  const [user, setUser] = useState("");
+  const [body, setBody] = useState("");
+  const [render, setRender] = useState(true);
   const [roomMessages, setRoomMessages] = useState([]);
 
   useEffect(() => {
-    //fetches information from a local API route set up on the server
-    async function getData(){
-      let res = await fetch(`http://localhost:8000/api/messages/${props.fetchPath}`)
+    //fetches information from a local API route set up on the server; fetchPath declared in the router in App.js
+    async function getData() {
+      let res = await fetch(
+        `http://localhost:8000/api/messages/${props.fetchPath}`
+      );
       let data = await res.json();
-      setRoomMessages(data)
+      setRoomMessages(data);
     }
-    getData()
-  }, [setRoomMessages, render, props.room]);
+    getData();
+  }, [render, props.fetchPath]);
 
+  
 
-async function submitForm(e) {
-  e.preventDefault()
-  fetch('http://localhost:8000/api/messages/new-message', {
-    headers: {
-      'Content-Type': 'application/json'
-    },
+  //fetchPath is the only 'stable' room-based variable; to avoid resetting header and room on refresh, attach them to fetchPath, which is attached to the page's path
+  if (props.fetchPath === "cars") {
+    props.setRoom("cars");
+    props.setRoomName("Car Talk");
+  } else if (props.fetchPath === "outerspace") {
+    props.setRoom("outerspace");
+    props.setRoomName("Outer Space");
+  } else {
+    props.setRoom("main");
+    props.setRoomName("Main");
+  }
+
+  async function submitForm(e) {
+    e.preventDefault();
+    e.target.reset();
+    // clear user input on submit
+    fetch("http://localhost:8000/api/messages/new-message", {
+      headers: {
+        "Content-Type": "application/json",
+      },
       method: "POST",
       body: JSON.stringify({
         user: user,
         body: body,
         date: new Date(),
-        room: props.room
-      })
+        room: props.room,
+      }),
     })
-    .then(function(res){ console.log(res) })
-    .catch(function(res){ console.log(res) })
-    if (render===true){
-      setRender(false)
-    } else {
-      setRender(true)
+      .then(function (res) {
+        //WAIT for fetch, then re-render
+        if (render === true) {
+          setRender(false);
+        } else {
+          setRender(true);
+        }
+      })
+      .catch(function (res) {
+        console.log(res);
+      });
+  }
+  
+
+  function getDate(date) {
+    const today = new Date();
+    const newDate = new Date(date);
+    let timeFrame = "am";
+    let minutes = newDate.getMinutes();
+    if (minutes<10){
+      minutes = '0' + minutes;
+    } 
+    let hours = newDate.getHours();
+    if (hours > 12) {
+      hours = hours - 12;
+      timeFrame = "pm";
     }
+    if (
+      newDate.getDate() === today.getDate() &&
+      newDate.getMonth() === today.getMonth()
+    ) {
+      return `${hours}:${minutes} ${timeFrame}`;
+    } else {
+      let day = `${newDate.getMonth()}/${newDate.getDate()}`;
+      return `${day} ${hours}:${minutes} ${timeFrame}`;
+    }
+    
   }
 
-     
-      return (
-        <div>
+  return (
+    <div>
       <table className="message-display">
         <tbody>
-        {roomMessages.map((message) => {
-          console.log(message.user)
-     return (
-       <tr key={message._id}>
-         <td>date: {message.date.slice(0, 10)}<br/>user name: {message.user}</td>
-         <td>{message.body}</td>
-
-        </tr>
-     )
-        })}
+          {roomMessages.map((message) => {
+            return (
+              <tr key={message._id}>
+                <td>
+                  {getDate(message.date)}
+                  <br />
+                  user name: {message.user}
+                </td>
+                <td>{message.body}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-      
-      
-       <form  method="POST" onSubmit={submitForm}>
-         <input type="text" placeholder="username" name="user" onChange={(e) => setUser(e.target.value)}/>
-         <input type="text" placeholder="message" name="body" onChange={(e) => setBody(e.target.value)}/>
-         <button type="submit">Send</button>
-       </form>
-     </div>)
-    
-  
-  
 
-  
+      <form method="POST" onSubmit={submitForm}>
+        <input
+          type="text"
+          placeholder="username"
+          name="user"
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="message"
+          name="body"
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
